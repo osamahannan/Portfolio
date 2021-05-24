e4ee9bf5a25babdf7649174791de9cea384320e7
osamahannan <83069516+osamahannan@users.noreply.github.com>
2021-05-24 11:17:14 +0530

    updated

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -16,21 +16,19 @@ const Project = () => {
                     <div className="heading"> Wether App</div>
                 </div>
 
-                <div className="time1">3rd January, 2021</div>
+                <div className="time1">15th february, 2021</div>
 
                 <div className="about-app">
                     <article>
                         <p>
-                            Created an app in React-JS to check if entered birthdate is Palindrome.
-                            If it is not palindrome, then it will show nearest palindrome date and by how many
-                            days it got missed.
+                            Created an app in React-JS to gives the weather information of a particular city in any country.
                         </p>
                     </article>
                 </div>
-                
+
                 <div className="sandgit-links">
                     <div className="sandbox-link">
-                        <a href="https://codesandbox.io/s/elated-lovelace-ykniy">Open App</a>
+                        <a href="https://codesandbox.io/s/zen-rosalind-8c92o">Open App</a>
                     </div>
 
                     <div className="github-link">
@@ -39,6 +37,159 @@ const Project = () => {
                 </div>
 
             </div>
+
+
+            <div className="project-container">
+                <div className="item-1">
+                    <div className="number"> 2.</div>
+                    <div className="heading"> Tic Tac Toe Game</div>
+                </div>
+
+                <div className="time1">5th february, 2021</div>
+
+                <div className="about-app">
+                    <article>
+                        <p>
+                            Created an app in Javascript. Tic-tac-toe is a simple, two-player game that,
+                            if played optimally by both players, will always result in a tie. The game is
+                            also called noughts and crosses or Xs and Os. A player wins a game if three
+                            marks of the same kind is connected in a line.
+                        </p>
+                    </article>
+                </div>
+
+                <div className="sandgit-links">
+                    <div className="sandbox-link">
+                        <a href="#">Open App</a>
+                    </div>
+
+                    <div className="github-link">
+                        <a href="#">View Source</a>
+                    </div>
+                </div>
+
+            </div>
+
+
+            <div className="project-container">
+                <div className="item-1">
+                    <div className="number">3.</div>
+                    <div className="heading"> Snake game</div>
+                </div>
+
+                <div className="time1">1st february, 2021</div>
+
+                <div className="about-app">
+                    <article>
+                        <p>
+                            Created an app in JavaScript. Snake game is a computer action game, whose goal
+                            is to control a snake to move and collect food in a map.
+                        </p>
+                    </article>
+                </div>
+
+                <div className="sandgit-links">
+                    <div className="sandbox-link">
+                        <a href="#">Open App</a>
+                    </div>
+
+                    <div className="github-link">
+                        <a href="#">View Source</a>
+                    </div>
+                </div>
+
+            </div>
+
+
+            <div className="project-container">
+                <div className="item-1">
+                    <div className="number"> 4.</div>
+                    <div className="heading"> TodoList App</div>
+                </div>
+
+                <div className="time1">20th January, 2021</div>
+
+                <div className="about-app">
+                    <article>
+                        <p>
+                            Created an app in React-JS to store your Todo list with a specific deadline.
+                        </p>
+                    </article>
+                </div>
+
+                <div className="sandgit-links">
+                    <div className="sandbox-link">
+                        <a href="https://codesandbox.io/s/busy-liskov-bouz2">Open App</a>
+                    </div>
+
+                    <div className="github-link">
+                        <a href="https://github.com/osamahannan/React_TodoList">View Source</a>
+                    </div>
+                </div>
+
+            </div>
+
+
+            <div className="project-container">
+                <div className="item-1">
+                    <div className="number"> 5.</div>
+                    <div className="heading"> iCoder Blog App</div>
+                </div>
+
+                <div className="time1">15th January, 2021</div>
+
+                <div className="about-app">
+                    <article>
+                        <p>
+                            Created an app using Bootstrap.It represents the blogs on various trending topics
+                            and technologies.
+                        </p>
+                    </article>
+                </div>
+
+                <div className="sandgit-links">
+                    <div className="sandbox-link">
+                        <a href="https://codesandbox.io/s/elated-lovelace-ykniy">Open App</a>
+                    </div>
+
+                    <div className="github-link">
+                        <a href="https://github.com/osamahannan/iCoder-blog">View Source</a>
+                    </div>
+                </div>
+
+            </div>
+
+
+            <div className="project-container">
+                <div className="item-1">
+                    <div className="number"> 6.</div>
+                    <div className="heading"> iClock </div>
+                </div>
+
+                <div className="time1">5th January, 2021</div>
+
+                <div className="about-app">
+                    <article>
+                        <p>
+                            Created an app in JavaSript. a simple digital clock which represent the current
+                            time zone and date of a particular country.
+                        </p>
+                    </article>
+                </div>
+
+                <div className="sandgit-links">
+                    <div className="sandbox-link">
+                        <a href="https://codesandbox.io/s/modest-wildflower-cbeir">Open App</a>
+                    </div>
+
+                    <div className="github-link">
+                        <a href="https://github.com/osamahannan/iClock">View Source</a>
+                    </div>
+                </div>
+
+            </div>
+
+
         </div>
     );
 }
